b7aa12e97c26e66c8b02431895271eb01c47dc42
webLiang <770698025@qq.com>
2018-05-31 17:50:59 +0800

    音乐项目提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    hello laing
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .hello
+    color:red
+</style>
